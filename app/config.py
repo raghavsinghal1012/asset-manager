b75a161b64c359658a5e0b6f7ec80a7d31eb4376
Raghav Singhal <raghav.singhal2001@gmail.com>
2026-07-05 22:54:53 +0530

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,8 +1,5 @@
+import os
 from pathlib import Path
-
-# -------------------------
-# Project Paths
-# -------------------------
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -10,31 +7,20 @@ UPLOAD_FOLDER = BASE_DIR / "uploads"
 GENERATED_FOLDER = BASE_DIR / "generated"
 DATABASE_PATH = BASE_DIR / "database" / "assets.db"
 
-# -------------------------
-# Company
-# -------------------------
-
 COMPANY_NAME = "Firm 1"
 
-# -------------------------
-# Database Backend
-# -------------------------
-
-DATABASE_BACKEND = "postgres"      # "sqlite" or "postgres"
+DATABASE_BACKEND = os.getenv("DATABASE_BACKEND", "postgres")
 
 SQLITE_DATABASE_URL = f"sqlite:///{DATABASE_PATH}"
 
-POSTGRES_DATABASE_URL = (
-    "postgresql://postgres:UNtEimvPDEiMdbtNEVgUfMRFYSYHqUKu@hayabusa.proxy.rlwy.net:10548/railway"
-)
+POSTGRES_DATABASE_URL = os.getenv("DATABASE_URL")
 
 if DATABASE_BACKEND == "postgres":
     DATABASE_URL = POSTGRES_DATABASE_URL
 else:
     DATABASE_URL = SQLITE_DATABASE_URL
 
-# -------------------------
-# Public URL
-# -------------------------
-
-BASE_URL = "http://127.0.0.1:8000"
+BASE_URL = os.getenv(
+    "BASE_URL",
+    "http://127.0.0.1:8000"
+)
